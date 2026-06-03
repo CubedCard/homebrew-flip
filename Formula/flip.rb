f74ea846e1e22177e67408603dcd81fbd6f9aa6b
Jip Derksen <78497126+CubedCard@users.noreply.github.com>
2026-06-03 15:15:19 +0200

    SHA updated

--- Formula/flip.rb
+++ Formula/flip.rb
@@ -2,7 +2,7 @@ class Flip < Formula
   desc "Print a random table-flipping emoji and copy it to your clipboard"
   homepage "https://github.com/CubedCard/flip"
   url "https://github.com/CubedCard/flip/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "09e56ab88e20ed20c9eb426a79073a38edbc0d0f51235a4a652df3baf32ff0e9  -"
+  sha256 "09e56ab88e20ed20c9eb426a79073a38edbc0d0f51235a4a652df3baf32ff0e9"
   license "MIT"
 
   def install
